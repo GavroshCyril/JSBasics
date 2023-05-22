826ca9d215150bf181f101f7fb8af5b8f273cade
Cyril <gavrosh.cyril@gmail.com>
2023-05-22 10:43:22 +0300

    feat: refactor of code

--- todoList/index.js
+++ todoList/index.js
@@ -1,88 +1,112 @@
 const inputBox = document.getElementById('input-box');
-const listContainer = document.getElementById('list-container'); 
+const listContainer = document.getElementById('list-container');
+
+window.addEventListener('load', () => {
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addTask();
+  });
+});
 
 const addTask = () => {
-    
-    if (inputBox.value === '') {
-        alert('Warning! You must write something!');
-    } else {
-
+  if (inputBox.value === '') {
+    alert('Warning! You must write something!');
+  } else {
     const newTask = {
-        id: new Date().getMilliseconds().toString(),
-        value: inputBox.value
+      id: new Date().getMilliseconds().toString(),
+      value: inputBox.value,
+      isChecked: false
     };
 
     const li = document.createElement('li');
-        li.setAttribute('data-id', newTask.id);
-        li.textContent = newTask.value;
-        listContainer.appendChild(li);
+    li.setAttribute('data-id', newTask.id);
+    li.textContent = newTask.value;
+    listContainer.appendChild(li);
 
-   /*  const span = document.createElement('span'); */
-        /* span.innerHTML = '\u00d7'; */
-/*         li.appendChild(span); */
-
+    const span = document.createElement('span');
+    span.innerHTML = '\u00d7';
+    li.appendChild(span);
+    listContainer.appendChild(li);
+    
     inputBox.value = '';
+
     saveData();
-    }
+  }
 };
 
+const toggleChecked = (id) => {
+  const tasks = getTasks();
+  const taskIndex = tasks.findIndex((task) => task.id === id);
+  tasks[taskIndex].isChecked = !tasks[taskIndex].isChecked;
+  saveTasks(tasks);
+};
+
+
 listContainer.addEventListener('click', (e) => {
-    if (e.target.tagName === 'LI') {
-        e.target.classList.toggle('checked');
-        saveData();
-    } else if (e.target.tagName === 'SPAN') {
-        const id = e.target.parentElement.getAttribute('data-id');
-        e.target.parentElement.remove();
-        deleteTask(id);
-        }
-    }, false
-);
+  const target = e.target;
+  switch (target.tagName) {
+    case 'LI':
+      target.classList.toggle('checked');
+      toggleChecked(target.getAttribute('data-id'));
+      break;
+    case 'SPAN':
+      const id = target.parentElement.getAttribute('data-id');
+      target.parentElement.remove();
+      deleteTask(id);
+      break;
+  }
+}, false);
+
+
 
 const deleteTask = (id) => {
-    const tasks = getTasks();
-    const updatedTasks = tasks.filter((task) => task.id !== id);
-    saveTasks(updatedTasks);
+  const tasks = getTasks().filter((task) => task.id !== id);
+  saveTasks(tasks);
 };
 
 const getTasks = () => {
-    const tasksString = localStorage.getItem('tasks');
-    if (tasksString) {
-        return JSON.parse(tasksString);
-    } else {
-        return [];
-    }
+  const tasksString = localStorage.getItem('tasks');
+  if (tasksString) {
+    return JSON.parse(tasksString);
+  } else {
+    return [];
+  }
 };
 
 const saveTasks = (tasks) => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 };
 
 const saveData = () => {
-    const tasks = [];
-    listContainer.querySelectorAll('li').forEach((li) => {
-
+  const tasks = [];
+  listContainer.querySelectorAll('li').forEach((li) => {
     const task = {
-        id: li.getAttribute('data-id'),
-        value: li.textContent
+      id: li.getAttribute('data-id'),
+      value: li.textContent,
+      isChecked: li.classList.contains('checked')
     };
 
-        tasks.push(task);
-    });
-    saveTasks(tasks);
+    tasks.push(task);
+  });
+  saveTasks(tasks);
 };
 
 const showTasks = () => {
-    const tasks = getTasks();
-    tasks.forEach((task) => {
-        const li = document.createElement('li');
-        li.setAttribute('data-id', task.id);
-        li.textContent = task.value;
-        listContainer.appendChild(li);
+  const tasks = getTasks();
+  tasks.forEach((task) => {
+    const li = document.createElement('li');
+    li.setAttribute('data-id', task.id);
+    li.textContent = task.value;
+    if (task.isChecked) {
+      li.classList.add('checked');
+    }
 
     const span = document.createElement('span');
-        span.innerHTML = '\u00d7';
-        li.appendChild(span);
-    });
+    span.innerHTML = '\u00d7';
+    li.appendChild(span);
+    listContainer.appendChild(li);
+  });
 };
 
 showTasks();
